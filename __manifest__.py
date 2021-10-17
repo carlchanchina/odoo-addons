8c52ab83d68aa63667249ba2b62c2d23d2ba3376
cyouen <carl@cyouendeiMac-Pro.local>
2021-10-17 21:12:13 +0800

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+# See LICENSE file for full copyright and licensing details.
+{
+    "name": "Custom Background",
+    "version": "15.0.0.0.1",
+    "author": "BizzAppDev",
+    "website": "http://www.bizzappdev.com",
+    "category": "GenericModules",
+    "depends": ["base", "web"],
+    "summary": "Custom  Background",
+    "images": ["images/image.png"],
+    "init_xml": [],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/ir_actions.xml",
+        "views/res_company_view.xml",
+        "views/report_template.xml",
+    ],
+    "installable": True,
+    "application": False,
+    "license": "Other proprietary",
+}
